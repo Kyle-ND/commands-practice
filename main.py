4761489b4e7535858341e00f7e47932c1efb110f
Genjutsu <wtc@pop-os.localdomain>
2024-09-11 16:08:17 +0200

    updated subtract and divide function

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ def multiply():
     ...
 
 def subtract():
-    ...
+    a = int(input())
+    b = int(input())
+    return a - b
 
 def divide():
-    ...
+    a = int(input())
+    b = int(input())
+    return a / b
