b519acc4aa93476ea692339a43ee4eca0c2a9001
wtc <wtc@pop-os.localdomain>
2024-09-11 16:16:42 +0200

    Added functionaluty

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-def add():
-    ...
+def add(*numbers):
+    sum_ = sum(numbers)
+    return sum
 
-def multiply():
-    ...
+def multiply(*numbers):
+    output = 1
+    for n in numbers:
+        output = n*output
+        return output
 
 def subtract():
     ...
 
 def divide():
     ...
+
+# var = 6
+# check = type(var)
+
+# # print(f'The variable is of type {check}')
+
+# if isinstance(var, str): print('OK')
+# else: var = str(var); print(f' {type(var)}')
+
